b63546b881191420ccb697f4478a035275d30098
han <hcsy1219@gmail.com>
2024-05-13 19:00:58 +0900

    프로젝트 생성

---
+++ src/main/java/soup/japopgg/JapopGgApplication.java
@@ -0,0 +1,13 @@
+package soup.japopgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapopGgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapopGgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soup/japopgg/JapopGgApplicationTests.java
@@ -0,0 +1,13 @@
+package soup.japopgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapopGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
